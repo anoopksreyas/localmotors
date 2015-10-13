4cfeee79a94c06a09b13338d97616d631101ed2c
Anoop Kolari <anoop.kolari@tarams.com>
2015-10-13 17:29:27 +0530

    updated car.view comment.

--- localmotors/apps/cars/views.py
+++ localmotors/apps/cars/views.py
@@ -1,30 +1,28 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from cars.models import Car
 from cars.forms import CarForm
 
-from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from cars.serializers import CarSerializer
 
-def carHome(request):
-    '''
-    Car home.
-    '''
+def car_home(request):
+    """ Car home. """
     if request.POST:
         form = CarForm(request.POST)
         if form.clean:
-            instance = form.save(commit = False)
+            instance = form.save(commit=False)
             instance.save() 
     form = CarForm()
     return render_to_response('car_home.html', locals(), context_instance=RequestContext(request))
 
 @api_view(['GET', 'POST'])
-def listCars(request):
+def list_cars():
+    """ DRF method to get list of cars. """
     cars = Car.objects.all()
-    serializer = CarSerializer(cars, many = True)
+    serializer = CarSerializer(cars, many=True)
     return Response(serializer.data)
 
 
