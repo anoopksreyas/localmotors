bd9681730717a4397e171b7dd0c6e173163dbedc
Anoop Kolari <anoop.kolari@tarams.com>
2015-10-19 15:39:58 +0530

    Testing unfuddle pylint hook.

--- localmotors/urls.py
+++ localmotors/urls.py
@@ -3,13 +3,9 @@ from django.contrib import admin
 from cars.views import car_home
 
 urlpatterns = patterns('',
-    # Examples:
     #url(r'^$', 'localmotors.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    
     url(r'^$', car_home, name='car_home'),
     url(r'^cars/$', car_home, name='car_home'),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('urls_api')),
 )
